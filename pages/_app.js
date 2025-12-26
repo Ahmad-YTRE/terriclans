b9c8758a4622ce0e2d72f006e96264a3573d7570
Ahmad_YTRE <ahmadnav.10319@gmail.com>
2025-12-26 11:06:24 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import { ThemeProvider } from '../contexts/ThemeContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider>
       <Head>
         <title>Terriclans</title>
         <meta name="description" content="Manage and join gaming clans" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
