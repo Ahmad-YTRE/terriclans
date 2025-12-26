4ee993c0a10c7c28f3f2b4035eedc1086af2221b
Ahmad_YTRE <ahmadnav.10319@gmail.com>
2025-12-26 10:34:03 +0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Terriclans</title>
+        <meta name="description" content="Manage and join gaming clans" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
